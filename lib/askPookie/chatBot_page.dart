de57f882628b6c1bcaf7c598e76154ebf4d76e13
riheb tlh <164495935+rihebtlh@users.noreply.github.com>
2025-02-12 18:17:27 +0100

    Update chatBot_page.dart
    removed comment

--- lib/askPookie/chatBot_page.dart
+++ lib/askPookie/chatBot_page.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-//hada li ykhdm
+
 class ChatPage extends StatefulWidget {
   const ChatPage({super.key});
 
